44711cdb231a65ab6bc50b2f04af34541f37ba50
lmw <alsdndjdrktl@gmail.com>
2023-08-05 18:26:42 +0900

    feat(#3): Google Maps BaseMap 컴포넌트
    - @react-google-maps/api
    - 추상화 BaseMap 컴포넌트(모든 옵션 수용)
    - jotai를 사용하여 Map 객체를 전역 상태로 사용할 수 있도록 해보자

---
+++ src/components/google/BaseMap.tsx
@@ -0,0 +1,60 @@
+import React, { useCallback, useMemo, useRef } from "react";
+import { googleMapAtom } from "../../stores/atoms";
+import { useSetAtom } from "jotai";
+import { GoogleMap } from "@react-google-maps/api";
+import { GOOGLE_MAPS } from "../../constants/googleMaps";
+
+interface Props extends Omit<GoogleMap["props"], "onLoad" | "onUnmount"> {
+  onLoadCallback?: (map: google.maps.Map) => void;
+  onUnmountCallback?:
+    | ((map: google.maps.Map) => void | Promise<void>)
+    | undefined;
+}
+
+function BaseMap(props: Props) {
+  const {
+    mapContainerStyle = GOOGLE_MAPS.defaultMapContainerStyle,
+    options = GOOGLE_MAPS.defaultOptions,
+    onLoadCallback,
+    onUnmountCallback,
+    center,
+    ...rest
+  } = props;
+  const setGoogleMap = useSetAtom(googleMapAtom);
+  const cacheCenter = useMemo(() => center, [center]);
+
+  const callbackRef = useRef({
+    onLoadCallbackRef: onLoadCallback,
+    onUnmountCallbackRef: onUnmountCallback,
+  });
+
+  const onLoadFn = useCallback(
+    function callback(map: google.maps.Map) {
+      setGoogleMap(map);
+
+      callbackRef.current.onLoadCallbackRef?.(map);
+    },
+    [setGoogleMap]
+  );
+
+  const onUnmountFn = useCallback(
+    function callback(map: google.maps.Map) {
+      setGoogleMap(null);
+      callbackRef.current.onUnmountCallbackRef?.(map);
+    },
+    [setGoogleMap]
+  );
+
+  return (
+    <GoogleMap
+      mapContainerStyle={mapContainerStyle}
+      options={options}
+      {...(cacheCenter && { center: cacheCenter })}
+      onLoad={onLoadFn}
+      onUnmount={onUnmountFn}
+      {...rest}
+    ></GoogleMap>
+  );
+}
+
+export default BaseMap;
--- src/pages/google-map.tsx
+++ src/pages/google-map.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import BaseMap from "../components/google/BaseMap";
+import { useJsApiLoader } from "@react-google-maps/api";
+import { useCurrentLocation } from "../hooks/useCurrentLocation";
 
 function GoogleMap() {
-  return <div>GoogleMap</div>;
+  const [center, setCenter] = useState<google.maps.LatLngLiteral>({
+    lat: 35.1795543,
+    lng: 129.0756416,
+  });
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+    language: "ko",
+  });
+
+  useCurrentLocation(
+    useCallback(({ lat, lng }) => {
+      setCenter({ lat, lng });
+    }, [])
+  );
+
+  return (
+    <>
+      {isLoaded ? (
+        <BaseMap zoom={13} center={center} onClick={console.log} />
+      ) : (
+        <div>loading... 🚀</div>
+      )}
+    </>
+  );
 }
 
 export default GoogleMap;

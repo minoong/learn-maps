382eaabad4b48ef0c070b9f757275850658b14a8
lmw <alsdndjdrktl@gmail.com>
2023-08-05 19:08:51 +0900

    feat(#6): 내가 사는 관악구를 기준으로 위도/경도 랜덤 생성 유틸
    - 관악구 그리고 구로구 주변의 위도 경도를 기준으로 랜덤 좌표 생성

--- src/pages/google-map.tsx
+++ src/pages/google-map.tsx
@@ -2,12 +2,12 @@ import React, { useCallback, useState } from "react";
 import BaseMap from "../components/google/BaseMap";
 import { useJsApiLoader } from "@react-google-maps/api";
 import { useCurrentLocation } from "../hooks/useCurrentLocation";
+import { getRandomCoordinates } from "../utils";
 
 function GoogleMap() {
-  const [center, setCenter] = useState<google.maps.LatLngLiteral>({
-    lat: 35.1795543,
-    lng: 129.0756416,
-  });
+  const [center, setCenter] = useState<google.maps.LatLngLiteral>(
+    getRandomCoordinates("Guro")
+  );
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
